0d9d01d0172d039ec11c9d0ea4281097cc112882
Laurie Hogg <lauriehogg1411@gmail.com>
2025-07-02 16:05:32 +0200

    Restruture project file names

--- songify/app.py
+++ songify/app.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import streamlit as st
 
-from songify import HarmonyGenerationParameters, MelodyExtractionParameters, SongifyApp
+from main import HarmonyGenerationParameters, MelodyExtractionParameters, SongifyApp
 
 melody_params = MelodyExtractionParameters()
 harmony_params = HarmonyGenerationParameters()
--- songify/main.py
+++ songify/main.py
@@ -1,19 +1,76 @@
 import torch
 import torchaudio
 
-import symusic
-import librosa
-import pesto
-
-import numpy as np
-import matplotlib.pyplot as plt
-
 import os
-
 import time
 
-from songify import melody, harmonise, utils
+from songify import melody, utils
 from songify import melody_harmonizer as mh
+
+from dataclasses import dataclass
+from typing import Any, List, Tuple
+
+
+@dataclass
+class MelodyExtractionParameters:
+    onset_detection: str = "option1"  # Options: "option1", "option2", "option3"
+    frame_size: float = 512
+    median_filter: float = 65.0
+    min_note_duration: float = 0.1
+    max_note_duration: float = 2.0
+
+
+@dataclass
+class HarmonyGenerationParameters:
+    chord_melody_congruence: float = 0.5
+    chord_variety: float = 0.5
+    harmonic_flow: float = 0.5
+    functional_harmony: float = 0.5
+
+
+class SongifyApp:
+    def __init__(
+        self,
+        melody_params: MelodyExtractionParameters = MelodyExtractionParameters(),
+        harmony_params: HarmonyGenerationParameters = HarmonyGenerationParameters(),
+    ):
+        self.melody_params = melody_params
+        self.harmony_params = harmony_params
+        self.audio = None
+        self.sample_rate = None
+
+    def load_audio(self, streamlit_file):
+        # file = os.path.join('data', 'Capn Holt 1.mp3')
+        streamlit_file.seek(0)  
+        self.audio, self.sample_rate = torchaudio.load(streamlit_file)
+        # print(f"Loading audio from {audio_path}")
+
+    # Extract melody from audio file, and return annotated melody (plot)
+    def extract_melody(self) -> List[Tuple[Any, Any, Any]]:
+        print("Extracting melody with parameters:", self.melody_params)
+        pass
+
+    def generate(
+        self,
+        melody_params: MelodyExtractionParameters,
+        harmony_params: HarmonyGenerationParameters,
+    ):
+        print("Generating music with parameters:")
+        print("Melody Parameters:", melody_params)
+        print("Harmony Parameters:", harmony_params)
+
+
+        pass
+
+    def get_generated_wav_file(self) -> str:
+        """
+        Returns the path to the generated WAV file.
+        """
+        return "path/to/generated.wav"
+
+    def get_generated_midi_file(self) -> str:
+        return "path/to/generated.mid"
+
 
 def main(merge: bool = True):
     # file = os.path.join('data', 'Capn Holt 2.mp3')
