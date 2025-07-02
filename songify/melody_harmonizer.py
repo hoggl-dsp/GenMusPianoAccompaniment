90b05978c69bb78d63ee420d1c1a1d0068a2fbfa
Zaka Elab <zakaelab@connectedsoma.com>
2025-07-02 14:50:30 +0200

    Remove and rename files

--- songify/melody_harmonizer.py
+++ songify/melody_harmonizer.py
@@ -1,10 +1,12 @@
 import random
 from dataclasses import dataclass
-
+from preferred_chord_transitions import preferred_transitions, chord_mappings
 import music21
 
+from songify import utils
 
-@dataclass(frozen=True)
+
+# @dataclass(frozen=True)
 class MelodyData:
     """
     A data class representing the data of a melody.
@@ -28,14 +30,22 @@ class MelodyData:
     """
 
     notes: list
-    duration: int = None  # Computed attribute
-    number_of_bars: int = None  # Computed attribute
+    duration: int
+    number_of_notes: int = None  # Computed attribute
 
-    def __post_init__(self):
-        object.__setattr__(
-            self, "duration", sum(duration for _, duration in self.notes)
-        )
-        object.__setattr__(self, "number_of_bars", self.duration // 4)
+    def __init__(self, notes, durations):
+        self.notes = notes
+        self.durations = durations
+        self.total_duration = sum(self.durations)
+        
+        self.number_of_notes = len(self.notes)
+        
+        assert self.number_of_notes == len(self.durations)
+
+    # def __post_init__(self):
+    #     object.__setattr__(self, "duration",
+    #                        sum(duration for _, duration in self.notes))
+    #     object.__setattr__(self, "number_of_notes", len(self.notes))
 
 
 class GeneticMelodyHarmonizer:
@@ -45,7 +55,7 @@ class GeneticMelodyHarmonizer:
     melody based on a fitness function.
 
     Attributes:
-        melody_data (MusicData): Data containing melody information.
+        melody_data (MelodyData): Data containing melody information.
         chords (list): Available chords for generating sequences.
         population_size (int): Size of the chord sequence population.
         mutation_rate (float): Probability of mutation in the genetic algorithm.
@@ -97,9 +107,7 @@ class GeneticMelodyHarmonizer:
             self._population = new_population
         best_chord_sequence = (
             self.fitness_evaluator.get_chord_sequence_with_highest_fitness(
-                self._population
-            )
-        )
+                self._population))
         return best_chord_sequence
 
     def _initialise_population(self):
@@ -124,7 +132,7 @@ class GeneticMelodyHarmonizer:
         """
         return [
             random.choice(self.chords)
-            for _ in range(self.melody_data.number_of_bars)
+            for _ in range(self.melody_data.number_of_notes)
         ]
 
     def _select_parents(self):
@@ -137,9 +145,9 @@ class GeneticMelodyHarmonizer:
         fitness_values = [
             self.fitness_evaluator.evaluate(seq) for seq in self._population
         ]
-        return random.choices(
-            self._population, weights=fitness_values, k=self.population_size
-        )
+        return random.choices(self._population,
+                              weights=fitness_values,
+                              k=self.population_size)
 
     def _create_new_population(self, parents):
         """
@@ -170,9 +178,9 @@ class GeneticMelodyHarmonizer:
         """
         new_population = []
         for i in range(0, self.population_size, 2):
-            child1, child2 = self._crossover(
-                parents[i], parents[i + 1]
-            ), self._crossover(parents[i + 1], parents[i])
+            child1, child2 = self._crossover(parents[i],
+                                             parents[i + 1]), self._crossover(
+                                                 parents[i + 1], parents[i])
             child1 = self._mutate(child1)
             child2 = self._mutate(child2)
             new_population.extend([child1, child2])
@@ -220,9 +228,8 @@ class FitnessEvaluator:
         preferred_transitions (dict): Preferred chord transitions.
     """
 
-    def __init__(
-        self, melody_data, chord_mappings, weights, preferred_transitions
-    ):
+    def __init__(self, melody_data, chord_mappings, weights,
+                 preferred_transitions):
         """
         Initialize the FitnessEvaluator with melody, chords, weights, and
         preferred transitions.
@@ -260,10 +267,9 @@ class FitnessEvaluator:
         Returns:
             float: The overall fitness score of the chord sequence.
         """
-        return sum(
-            self.weights[func] * getattr(self, f"_{func}")(chord_sequence)
-            for func in self.weights
-        )
+        return sum(self.weights[func] *
+                   getattr(self, f"_{func}")(chord_sequence)
+                   for func in self.weights)
 
     def _chord_melody_congruence(self, chord_sequence):
         """
@@ -283,18 +289,25 @@ class FitnessEvaluator:
                 chord sequence and the melody, normalized by the melody's
                 duration.
         """
-        score, melody_index = 0, 0
-        for chord in chord_sequence:
-            bar_duration = 0
-            while bar_duration < 4 and melody_index < len(
-                self.melody_data.notes
-            ):
-                pitch, duration = self.melody_data.notes[melody_index]
-                if pitch[0] in self.chord_mappings[chord]:
+        score = 0
+        
+        # print(list(enumerate(
+        #         zip(chord_sequence, self.melody_data.notes, self.melody_data.durations))))
+        
+        for i, (chord, (pitch, duration)) in enumerate(
+                zip(chord_sequence, zip(self.melody_data.notes, self.melody_data.durations))):
+            start = max(0, i - 2)
+            end = min(len(self.melody_data.notes), i + 3)
+            window_notes = self.melody_data.notes[start:end]
+            window_pitches = [note[0] for note in window_notes]
+
+            if any(p in self.chord_mappings[chord] for p in window_pitches):
+                try:
                     score += duration
-                bar_duration += duration
-                melody_index += 1
-        return score / self.melody_data.duration
+                except:
+                    print(f'chord_sequence: {chord_sequence}')
+
+        return score / self.melody_data.total_duration
 
     def _chord_variety(self, chord_sequence):
         """
@@ -332,38 +345,15 @@ class FitnessEvaluator:
         """
         score = 0
         for i in range(len(chord_sequence) - 1):
+            current_chord = chord_sequence[i]
             next_chord = chord_sequence[i + 1]
-            if next_chord in self.preferred_transitions[chord_sequence[i]]:
-                score += 1
+            if current_chord in self.preferred_transitions:
+                if next_chord in self.preferred_transitions[chord_sequence[i]]:
+                    score += 1
         return score / (len(chord_sequence) - 1)
 
-    def _functional_harmony(self, chord_sequence):
-        """
-        Evaluates the chord sequence based on principles of functional harmony.
-        This function checks for the presence of key harmonic functions such as
-        the tonic at the beginning and end of the sequence and the presence of
-        subdominant and dominant chords. Adherence to these harmonic
-        conventions is rewarded with a higher score.
 
-        Parameters:
-            chord_sequence (list): The chord sequence to evaluate.
-
-        Returns:
-            float: A score representing the extent to which the sequence
-                adheres to traditional functional harmony, normalized by
-                the number of checks performed.
-        """
-        score = 0
-        if chord_sequence[0] in ["C", "Am"]:
-            score += 1
-        if chord_sequence[-1] in ["C"]:
-            score += 1
-        if "F" in chord_sequence and "G" in chord_sequence:
-            score += 1
-        return score / 3
-
-
-def create_score(melody, chord_sequence, chord_mappings):
+def create_score(melody, starts, chord_sequence, chord_mappings):
     """
     Create a music21 score with a given melody and chord sequence.
 
@@ -378,27 +368,23 @@ def create_score(melody, chord_sequence, chord_mappings):
     """
     # Create a Score object
     score = music21.stream.Score()
-
-    # Create the melody part and add notes to it
     melody_part = music21.stream.Part()
-    for note_name, duration in melody:
-        melody_note = music21.note.Note(note_name, quarterLength=duration)
+    chord_part = music21.stream.Part()
+
+    current_offset = 0
+    for (note_name,
+         duration), start, chord_name in zip(melody, starts, chord_sequence):
+        melody_note = music21.note.Note(note_name)
+        melody_note.seconds = duration
+        melody_note.offset = start
         melody_part.append(melody_note)
 
-    # Create the chord part and add chords to it
-    chord_part = music21.stream.Part()
-    current_duration = 0  # Track the duration for chord placement
+        chord_notes = chord_mappings.get(chord_name, [])
+        chord = music21.chord.Chord(chord_notes, quarterLength=duration)
+        chord.offset = current_offset
+        chord_part.append(chord)
 
-    for chord_name in chord_sequence:
-        # Translate chord names to note lists
-        chord_notes_list = chord_mappings.get(chord_name, [])
-        # Create a music21 chord
-        chord_notes = music21.chord.Chord(
-            chord_notes_list, quarterLength=4
-        )  # Assuming 4/4 time signature
-        chord_notes.offset = current_duration
-        chord_part.append(chord_notes)
-        current_duration += 4  # Increase by 4 beats
+        current_offset += duration
 
     # Append parts to the score
     score.append(melody_part)
@@ -407,79 +393,114 @@ def create_score(melody, chord_sequence, chord_mappings):
     return score
 
 
+def midi_note_to_note_string(note_number):
+    note_names = [
+        'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'
+    ]
+    note = note_names[note_number % 12]
+    octave = (note_number // 12) - 1  # MIDI standard: C4 = 60
+    return f"{note}{octave}"
+
+
+def note_string_to_midi_note(note_string):
+    note_names = {
+        'C': 0,
+        'C#': 1,
+        'Db':1,
+        'D': 2,
+        'D#': 3,
+        'Eb':3,
+        'E': 4,
+        'F': 5,
+        'F#': 6,
+        'Gb':6,
+        'G': 7,
+        'G#': 8,
+        'Ab':8,
+        'A': 9,
+        'A#': 10,
+        'Bb':10,
+        'B': 11
+    }
+    
+    for name in sorted(note_names.keys(), key=len, reverse=True):
+        if note_string.startswith(name):
+            try:
+                octave = int(note_string[len(name):])
+            except:
+                octave = 4
+
+            return (octave + 1) * 12 + note_names[name]
+    raise ValueError(f"Invalid note string: {note_string}")
+
+
+def chord_strings_to_midi_chords(chords):
+    # Chord string to note strings list
+    chords = [chord_mappings[chord] for chord in chords]
+
+    # Note string list to MIDI integer note list
+
+    chords = [[note_string_to_midi_note(string_note) for string_note in notes]
+              for notes in chords]
+    
+    return chords
+
+
 def main():
 
-    twinkle_twinkle_melody = [
-        ("C5", 1),
-        ("C5", 1),
-        ("G5", 1),
-        ("G5", 1),
-        ("A5", 1),
-        ("A5", 1),
-        ("G5", 2),  # Twinkle, twinkle, little star,
-        ("F5", 1),
-        ("F5", 1),
-        ("E5", 1),
-        ("E5", 1),
-        ("D5", 1),
-        ("D5", 1),
-        ("C5", 2),  # How I wonder what you are!
-        ("G5", 1),
-        ("G5", 1),
-        ("F5", 1),
-        ("F5", 1),
-        ("E5", 1),
-        ("E5", 1),
-        ("D5", 2),  # Up above the world so high,
-        ("G5", 1),
-        ("G5", 1),
-        ("F5", 1),
-        ("F5", 1),
-        ("E5", 1),
-        ("E5", 1),
-        ("D5", 2),  # Like a diamond in the sky.
-        ("C5", 1),
-        ("C5", 1),
-        ("G5", 1),
-        ("G5", 1),
-        ("A5", 1),
-        ("A5", 1),
-        ("G5", 2),  # Twinkle, twinkle, little star,
-        ("F5", 1),
-        ("F5", 1),
-        ("E5", 1),
-        ("E5", 1),
-        ("D5", 1),
-        ("D5", 1),
-        ("C5", 2)  # How I wonder what you are!
-    ]
+    data = [(57, 0.15000000596046448, 0.44999999999999996, 1.0),
+            (55, 0.6000000238418579, 0.15000000000000002, 1.0),
+            (57, 0.75, 0.05, 1.0),
+            (58, 0.800000011920929, 0.15000000000000002, 1.0),
+            (52, 0.949999988079071, 0.05, 1.0),
+            (50, 1.0, 0.3, 0.9999992847442627),
+            (52, 1.2999999523162842, 0.05, 1.0),
+            (54, 1.350000023841858, 0.15000000000000002, 1.0),
+            (51, 2.049999952316284, 0.2, 1.0), (49, 2.25, 0.05, 1.0),
+            (47, 2.299999952316284, 0.1, 0.9999985694885254),
+            (52, 2.4000000953674316, 0.1, 1.0), (51, 2.5, 0.05, 1.0),
+            (48, 2.549999952316284, 0.1, 1.0),
+            (47, 2.6500000953674316, 0.1, 1.0), (46, 2.75, 0.05, 1.0),
+            (48, 2.9000000953674316, 0.15000000000000002, 1.0),
+            (47, 3.049999952316284, 0.05, 1.0),
+            (55, 3.200000047683716, 0.1, 1.0),
+            (49, 3.299999952316284, 0.05, 1.0),
+            (47, 3.450000047683716, 0.3, 1.0),
+            (48, 3.75, 0.15000000000000002, 1.0),
+            (53, 3.9000000953674316, 0.1, 1.0),
+            (44, 4.0, 0.15000000000000002, 0.4803946912288666),
+            (45, 4.150000095367432, 0.1, 1.0), (46, 4.25, 0.05, 1.0),
+            (50, 4.300000190734863, 0.05, 1.0),
+            (52, 4.349999904632568, 0.1, 1.0),
+            (41, 4.550000190734863, 0.2, 0.9998537302017212),
+            (39, 4.75, 0.05, 0.9999998807907104),
+            (51, 5.599999904632568, 0.1, 1.0), (53, 6.0, 0.1, 1.0),
+            (52, 6.099999904632568, 0.05, 1.0),
+            (51, 6.150000095367432, 0.05, 1.0),
+            (50, 6.199999809265137, 0.2, 0.9975391626358032),
+            (49, 6.400000095367432, 0.05, 0.9998569488525391),
+            (48, 6.449999809265137, 0.1, 1.0),
+            (47, 6.550000190734863, 0.1, 0.9991744160652161),
+            (43, 7.0, 0.15000000000000002, 1.0),
+            (42, 7.150000095367432, 0.1, 0.9995341300964355),
+            (41, 7.25, 0.05, 1.0), (40, 7.300000190734863, 0.1, 1.0),
+            (42, 7.400000095367432, 0.1, 1.0),
+            (50, 7.599999904632568, 0.1, 0.9998584985733032)]
+
+    pitch = [midi_note_to_note_string(note[0]) for note in data]
+    starts = [note[1] for note in data]
+    durations = [note[2] for note in data]
+    velocities = [note[3] for note in data]
+
     weights = {
-        "chord_melody_congruence": 0.01,
-        "chord_variety": 0.01,
-        "harmonic_flow": 0.01,
-        "functional_harmony": 0.97
-    }
-    chord_mappings = {
-        "C": ["C", "E", "G"],
-        "Dm": ["D", "F", "A"],
-        "Em": ["E", "G", "B"],
-        "F": ["F", "A", "C"],
-        "G": ["G", "B", "D"],
-        "Am": ["A", "C", "E"],
-        "Bdim": ["B", "D", "F"]
-    }
-    preferred_transitions = {
-        "C": ["G", "Am", "F"],
-        "Dm": ["G", "Am"],
-        "Em": ["Am", "F", "C"],
-        "F": ["C", "G"],
-        "G": ["Am", "C"],
-        "Am": ["Dm", "Em", "F", "C"],
-        "Bdim": ["F", "Am"]
+        "chord_melody_congruence": 0.3,
+        "chord_variety": 0.2,
+        "harmonic_flow": 0.5,
     }
 
     # Instantiate objects for generating harmonization
-    melody_data = MelodyData(twinkle_twinkle_melody)
+    
+    melody_data = MelodyData(pitch, durations)
     fitness_evaluator = FitnessEvaluator(
         melody_data=melody_data,
         weights=weights,
@@ -489,20 +510,19 @@ def main():
     harmonizer = GeneticMelodyHarmonizer(
         melody_data=melody_data,
         chords=list(chord_mappings.keys()),
-        population_size=100,
+        population_size=100,  #TODO: Change this to increase
         mutation_rate=0.05,
         fitness_evaluator=fitness_evaluator,
     )
 
     # Generate chords with genetic algorithm
-    generated_chords = harmonizer.generate(generations=1000)
+    harmony = harmonizer.generate(generations=1000)
 
-    # Render to music21 score and show it
-    music21_score = create_score(
-        twinkle_twinkle_melody, generated_chords, chord_mappings
-    )
-    music21_score.show()
+    # print(f'harmony: {harmony}')
 
+    harmony = zip(chord_strings_to_midi_chords(harmony), starts, durations, velocities)
+
+    return harmony
 
 if __name__ == "__main__":
     main()
